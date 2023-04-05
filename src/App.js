1d8dded511a31fca4c481255efc9c640ae5ab6db
deergod <CommonTroll@live.co.uk>
2023-04-05 01:10:17 +0100

    updating project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
+import { useState } from 'react';
 import './App.css';
+import TaskItem from './components/TaskItem';
 
 const App = () => {
-    return (
-        <div className="App">
+    const [taskList, setTaskList] = useState([]);
+    
+    const getTime = () => {
+        const date = new Date();
+        return `${date.getHours()}:${date.getMinutes()} - ${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+    }
 
+    const idGen = () => {
+        return Math.floor(((Math.random() + 1) * 100000000)).toString(16);
+    }
+    
+    const taskAdd = (e) => {
+        e.preventDefault();
+
+        const form = e.target;
+        let taskObj = new FormData(form);
+        taskObj = Object.fromEntries(taskObj.entries());
+        const task = taskObj.todoItemInput;
+
+        setTaskList([...taskList, {id: idGen(), item: task, date: getTime()}]);
+        console.log(taskList);
+    }
+
+    const taskRemove = (id) => {
+        setTaskList(taskList.filter(item => item.id !== id))
+    }
+
+
+    return (
+        <div method="post" className="App">
+            <h1>To Do List</h1>
+            <form onSubmit={taskAdd}>
+                <label form="todo-item-input">task:</label>
+                <input type="text" id="todo-item-input" name="todoItemInput" />
+                <input type="submit" value="Add" />
+            </form>
+
+            {taskList.length === 0 ? 
+                <h2>Add some tasks!</h2> :
+                taskList.map((task, index) => {
+                    return (
+                        <TaskItem key={index} task={task} taskRemove={taskRemove} />
+                    )
+                })}
         </div>
     );
 }
---
+++ src/components/TaskItem.js
@@ -0,0 +1,19 @@
+import { Textfit } from "react-textfit";
+
+const TaskItem = (props) => {
+    return (
+        <div className="taskContainer">
+            <div className="taskContent">
+                <Textfit>{props.task.item}</Textfit>
+            </div>
+
+            <div className="taskControls">
+                <p>Date added: {props.task.date}</p>
+                <button>Edit</button>
+                <button onClick={() => props.taskRemove(props.task.id)}>Remove</button>
+            </div>
+        </div>
+    );
+}
+
+export default TaskItem;
